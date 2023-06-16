ac94bf7c9fb00aa96868a0e22372e3488367af6a
Sebastian Semeniuc <xsebyyy@gmail.com>
2023-06-16 19:24:22 +0300

    add more dependencies and code workspace file

--- indexing-service/app.py
+++ indexing-service/app.py
@@ -1,5 +1,6 @@
 import os
 import time
+import openai
 import pandas as pd
 from flask_cors import CORS
 from flask import Flask, jsonify, request
@@ -9,11 +10,25 @@ app.config['UPLOAD_FOLDER'] = "upload"
 
 CORS(app)
 
+def get_embedding(text: str, model="text-embedding-ada-002"):
+    """Get embedding from text using text-embedding-ada-002 model."""
+
 
 @app.route("/ping", methods=["GET"])
 def ping():
     response = {"message": "pong!"}
     return jsonify(response)
+
+
+@app.route("/index", methods=["POST"])
+def index():
+    # we want to detect what type of data we get here
+    # so we will go with different steps for each type
+    # for indexing
+
+    # 1. get the data
+    pass
+
 
 
 if __name__ == "__main__":
