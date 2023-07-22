d9f833b9421a706b76b032dec2f63cf223f04fbd
octavi42 <53470992+octavi42@users.noreply.github.com>
2023-07-22 12:04:46 +0300

    adding backaend updates to backend brench

--- indexing-service/services/openai_service.py
+++ indexing-service/services/openai_service.py
@@ -1,6 +1,5 @@
 import openai
 
-
 def get_embedding(text: str, model="text-embedding-ada-002"):
     """Get embedding from text using text-embedding-ada-002 model."""
     return openai.Embedding.create(input=[text], model=model)['data'][0]['embedding']
@@ -11,7 +10,7 @@ def get_transcription(file, model="whisper-1"):
 
     openai.api_key = "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"
 
-    print(openai.api_key)
+    # print(openai.api_key)
 
     # use openai whisper api to get transcription of the audio file
     return openai.Audio.transcribe(file=file, model=model)
