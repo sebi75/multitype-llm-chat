fda67445b2bb96793df8dc933cd78d4a4101bbc6
octavi42 <53470992+octavi42@users.noreply.github.com>
2023-07-22 12:22:00 +0300

    changing openai access

--- indexing-service/services/openai_service.py
+++ indexing-service/services/openai_service.py
@@ -1,4 +1,6 @@
 import openai
+import os
+from dotenv import load_dotenv
 
 def get_embedding(text: str, model="text-embedding-ada-002"):
     """Get embedding from text using text-embedding-ada-002 model."""
@@ -8,9 +10,9 @@ def get_embedding(text: str, model="text-embedding-ada-002"):
 def get_transcription(file, model="whisper-1"):
     print("Getting transcription from openai...")
 
-    openai.api_key = "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"
+    load_dotenv()
 
-    # print(openai.api_key)
+    openai.api_key = os.getenv('OPENAI_API_KEY')
 
     # use openai whisper api to get transcription of the audio file
     return openai.Audio.transcribe(file=file, model=model)
